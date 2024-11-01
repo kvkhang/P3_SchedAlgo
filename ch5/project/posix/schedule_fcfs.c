51fee5d3e137e5eec93d756e8ac385059a2f8d3f
Nosua <dathee2@gmail.com>
2024-11-01 16:27:42 -0700

    start to finish

---
+++ ch5/project/posix/schedule_fcfs.c
@@ -0,0 +1,55 @@
+#include "schedulers.h"
+#include "list.h"
+#include "task.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node **g_head = NULL;
+
+// add a task to the list
+void add(char *name, int priority, int burst)
+{
+    Task *currTask = (Task *)malloc(sizeof(Task));
+    currTask->name = name;
+    currTask->priority = priority;
+    currTask->burst = burst;
+    insert(&g_head, currTask);
+}
+
+bool comesBefore(char *a, char *b) { return strcmp(a, b) < 0; }
+
+// based on traverse from list.c
+// finds the task whose name comes first in dictionary
+Task *pickNextTask()
+{
+    // if list is empty, nothing to do
+    if (!g_head)
+        return NULL;
+
+    struct node *temp;
+    temp = g_head;
+    Task *best_sofar = temp->task;
+
+    while (temp != NULL)
+    {
+        if (comesBefore(temp->task->name, best_sofar->name))
+            best_sofar = temp->task;
+        temp = temp->next;
+    }
+    // delete the node from list, Task will get deleted later
+    delete (&g_head, best_sofar);
+    return best_sofar;
+}
+// invoke the scheduler
+void schedule()
+{
+    int totalTime_ = 0;
+    while (g_head != NULL)
+    {
+        Task *nextTask_ = pickNextTask();
+        totalTime_ += nextTask_->burst;
+        printf("Running Task = [%s] [%d] [%d] for %d units.\n        Time is now: %d\n", nextTask_->name, nextTask_->priority, nextTask_->burst, nextTask_->burst, totalTime_);
+    }
+}
---
+++ ch5/project/posix/schedule_priority_rr.c
@@ -0,0 +1,104 @@
+#include "schedulers.h"
+#include "list.h"
+#include "task.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node **g_head = NULL;
+bool rrOn = false;
+// add a task to the list
+void add(char *name, int priority, int burst)
+{
+    Task *currTask = (Task *)malloc(sizeof(Task));
+    currTask->name = name;
+    currTask->tid = 0;
+    currTask->priority = priority;
+    currTask->burst = burst;
+    insert(&g_head, currTask);
+}
+
+bool comesBefore(char *a, char *b) { return strcmp(a, b) < 0; }
+
+// based on traverse from list.c
+// finds the task whose name comes first in dictionary
+Task *pickNextTask()
+{
+    // if list is empty, nothing to do
+    if (!g_head)
+        return NULL;
+
+    struct node *temp;
+    temp = g_head;
+    Task *best_sofar = temp->task;
+    while (temp != NULL)
+    {
+        if (temp->task->priority > best_sofar->priority)
+        {
+            best_sofar = temp->task;
+            rrOn = false;
+        }
+        else if (temp->task->priority == best_sofar->priority && temp->task->tid < best_sofar->tid)
+        {
+            best_sofar = temp->task;
+            rrOn = true;
+        }
+        else if (temp->task->tid == best_sofar->tid && temp->task->priority == best_sofar->priority && comesBefore(temp->task->name, best_sofar->name))
+        {
+            best_sofar = temp->task;
+            rrOn = true;
+        }
+        temp = temp->next;
+    }
+    // delete the node from list, Task will get deleted later
+    best_sofar->tid++;
+    if (best_sofar->burst <= 0)
+        delete (&g_head, best_sofar);
+    return best_sofar;
+}
+// invoke the scheduler
+void schedule()
+{
+    int totalTime_ = 0;
+    int runTime_ = 0;
+    while (g_head != NULL)
+    {
+        Task *nextTask_ = pickNextTask();
+        if (rrOn)
+        {
+            if (nextTask_->burst > 10)
+            {
+                totalTime_ += 10;
+                runTime_ = 10;
+            }
+            else
+            {
+                totalTime_ += nextTask_->burst;
+                runTime_ = nextTask_->burst;
+            }
+        }
+        else
+        {
+            totalTime_ += nextTask_->burst;
+            runTime_ = nextTask_->burst;
+        }
+
+        printf("Running Task = [%s] [%d] [%d] for %d units.\n        Time is now: %d\n", nextTask_->name, nextTask_->priority, nextTask_->burst, runTime_, totalTime_);
+
+        if (rrOn)
+        {
+            nextTask_->burst -= 10;
+        }
+        else
+        {
+            nextTask_->burst = 0;
+        }
+
+        if (nextTask_->burst <= 0)
+        {
+            delete (&g_head, nextTask_);
+            rrOn = false;
+        }
+    }
+}
